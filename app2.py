10aa3ee4b6bbffc79044492c2bff7888c66424c9
benjamin-bichon <66991514+benjamin-bichon@users.noreply.github.com>
2024-02-21 14:41:15 +0100

    v3

--- app2.py
+++ app2.py
@@ -30,4 +30,4 @@ def match_details(match_id):
     return render_template('match_details.html', match_details=match_details)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
